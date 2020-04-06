14d81296ab1a4dabd8438ecbcb83184ba7d32a98
kadrianne <kristine.a.du@gmail.com>
2020-04-06 16:09:26 -0600

    Solve another way.

--- app.js
+++ app.js
@@ -1,133 +1,161 @@
-const buttons = document.querySelectorAll('span')
-const buttonsList = Array.from(buttons)
+const numbers = document.querySelectorAll('span:not(.operator)')
 const screen = document.querySelector('#screen')
 const clear = document.querySelector('#clear')
 const equals = document.querySelector('#equals')
 const operators = document.querySelectorAll('.operator')
-const operatorList = Array.from(operators)
 let expression = ''
 
-displayOnClick()
-displayOnPress()
+numbers.forEach(addNumberListener)
 
-clear.addEventListener('click', clearScreen)
-document.addEventListener('keydown', pressEscape)
-
-equals.addEventListener('click', equalsButton)
-document.addEventListener('keydown', pressEnter)
-
-function displayOnClick(){
-    buttonsList.forEach(button => {
-        button.addEventListener('click', () => {
-            if (button == equals) {
-                screen.innerText = expression
-            } else {
-                expression += button.innerText
-                screen.innerText = expression
-            }
-        })
-    })
+function addNumberListener(number){
+    number.addEventListener('click', appendNumber)
 }
 
-function displayOnPress(){
-    document.addEventListener('keydown', () => {
-        switch (event.key) {
-            case '1':
-                expression += '1'
-                return screen.innerText = expression;
-            case '2':
-                expression += '2'
-                return screen.innerText = expression;
-            case '3':
-                expression += '3'
-                return screen.innerText = expression;
-            case '4':
-                expression += '4'
-                return screen.innerText = expression;
-            case '5':
-                expression += '5'
-                return screen.innerText = expression;
-            case '6':
-                expression += '6'
-                return screen.innerText = expression;
-            case '7':
-                expression += '7'
-                return screen.innerText = expression;
-            case '8':
-                expression += '8'
-                return screen.innerText = expression;
-            case '9':
-                expression += '9'
-                return screen.innerText = expression;
-            case '0':
-                expression += '0'
-                return screen.innerText = expression;
-            case '+':
-                expression += '+'
-                return screen.innerText = expression;
-            case '-':
-                expression += '-'
-                return screen.innerText = expression;
-            case '*':
-                expression += 'x'
-                return screen.innerText = expression;
-            case '/':
-                expression += '÷'
-                return screen.innerText = expression;
-        }
-    })
+function appendNumber(){
+    screen.innerText += event.target.innerText
 }
 
-function clearScreen(){
-    screen.innerText = ''
-    resetCalculator()
+operators.forEach(addOperatorListener)
+
+function addOperatorListener(operator){
+    operator.addEventListener('click', performOperation)
 }
 
-function equalsButton(){
-    evaluateExpression(expression)
-    resetCalculator()
-}
-
-function resetCalculator(){
-    expression = ''
-    answer = ''
-}
-
-function evaluateExpression(expression){
-    let convertedExpression = convertOperators(expression)
-    if (isValidExpression(convertedExpression) == false){
-        screen.innerText = 'Error'
-    } else {
-        let answer = eval(convertedExpression)
-        screen.innerText = answer
+function performOperation(){
+    const operations = {
+        "C"(){screen.innerText = ""},
+        "="(){screen.innerText = eval(screen.innerText)},
+        "x"(){screen.innerText += "*"},
+        "÷"(){screen.innerText += "/"},
+        "default"(character){screen.innerText += character}
+    }
+    const screenCharacters = screen.innerText.split("")
+    const lastCharacter = screenCharacters[screenCharacters.length-1]
+    if (/[0-9]/.test(lastCharacter)) {
+        const operation = operations[event.target.innerText] || operations["default"]
+        operation(event.target.innerText)
     }
 }
 
-function convertOperators(expression){
-    expressionWithMultiply = expression.replace(/x/g,'*')
-    expressionWithDivision = expressionWithMultiply.replace(/÷/g,'/')
-    return expressionWithDivision
-}
+// buttonsList.forEach(button => {
+//     button.addEventListener('click', displayOnClick(button))
+// })
+// document.addEventListener('keydown', displayOnPress)
 
-function isValidExpression(expression){
-    const badOperators = ['/*','*/','*+','/+','+/','+*','-+','-*','-/','/0']
-    let checkedExpression = expression
-    badOperators.forEach(operator => {
-        if (expression.includes(operator) == true){
-            checkedExpression = false
-        }
-    })
-    return checkedExpression
-}
+// clear.addEventListener('click', clearScreen)
+// document.addEventListener('keydown', pressEscape)
 
-function pressEscape(){
-    if (event.key == 'Escape') {
-        clearScreen()
-    }
-}
+// equals.addEventListener('click', equalsButton)
+// document.addEventListener('keydown', pressEnter)
 
-function pressEnter(){
-    if (event.key == 'Enter') {
-        equalsButton()
-    }  
-}
+// function displayOnClick(button){
+//     return function(){
+//         if (button == equals) {
+//             screen.innerText = expression
+//         } else {
+//             expression += button.innerText
+//             screen.innerText = expression
+//         }
+//     }
+// }
+
+// function displayOnPress(){
+//     switch (event.key) {
+//         case '1':
+//             expression += '1'
+//             return screen.innerText = expression;
+//         case '2':
+//             expression += '2'
+//             return screen.innerText = expression;
+//         case '3':
+//             expression += '3'
+//             return screen.innerText = expression;
+//         case '4':
+//             expression += '4'
+//             return screen.innerText = expression;
+//         case '5':
+//             expression += '5'
+//             return screen.innerText = expression;
+//         case '6':
+//             expression += '6'
+//             return screen.innerText = expression;
+//         case '7':
+//             expression += '7'
+//             return screen.innerText = expression;
+//         case '8':
+//             expression += '8'
+//             return screen.innerText = expression;
+//         case '9':
+//             expression += '9'
+//             return screen.innerText = expression;
+//         case '0':
+//             expression += '0'
+//             return screen.innerText = expression;
+//         case '+':
+//             expression += '+'
+//             return screen.innerText = expression;
+//         case '-':
+//             expression += '-'
+//             return screen.innerText = expression;
+//         case '*':
+//             expression += 'x'
+//             return screen.innerText = expression;
+//         case '/':
+//             expression += '÷'
+//             return screen.innerText = expression;
+//     }
+// }
+
+// function clearScreen(){
+//     screen.innerText = ''
+//     resetCalculator()
+// }
+
+// function equalsButton(){
+//     evaluateExpression(expression)
+//     resetCalculator()
+// }
+
+// function resetCalculator(){
+//     expression = ''
+//     answer = ''
+// }
+
+// function evaluateExpression(expression){
+//     let convertedExpression = convertOperators(expression)
+//     if (isValidExpression(convertedExpression) == false){
+//         screen.innerText = 'Error'
+//     } else {
+//         let answer = eval(convertedExpression)
+//         screen.innerText = answer
+//     }
+// }
+
+// function convertOperators(expression){
+//     expressionWithMultiply = expression.replace(/x/g,'*')
+//     expressionWithDivision = expressionWithMultiply.replace(/÷/g,'/')
+//     return expressionWithDivision
+// }
+
+// function isValidExpression(expression){
+//     const badOperators = ['/*','*/','*+','/+','+/','+*','-+','-*','-/','/0']
+//     let checkedExpression = expression
+//     badOperators.forEach(operator => {
+//         if (expression.includes(operator) == true){
+//             checkedExpression = false
+//         }
+//     })
+//     return checkedExpression
+// }
+
+// function pressEscape(){
+//     if (event.key == 'Escape') {
+//         clearScreen()
+//     }
+// }
+
+// function pressEnter(){
+//     if (event.key == 'Enter') {
+//         equalsButton()
+//     }  
+// }
